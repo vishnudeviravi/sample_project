e94a3c33cc64adc047bad1a5da31b6ecdc2d42b8
vishnudeviravi <ivishnudr@gmail.com>
2024-11-11 20:11:36 +0530

    adding comments

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const sum = require('./index');
 
 console.log(sum(234, 432));
+
+// git status - to check current status
+// git add .  - to select all files to be commited
+// git commit -m "message"  - to save the file to git
+// git log
