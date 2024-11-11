bcba189ed58e226db2289d327316665389533f61
vishnudeviravi <ivishnudr@gmail.com>
2024-11-11 20:26:33 +0530

    ADDED SUB FUNCTION

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const sum = (fNum, sNum) => {
   return fNum + sNum;
 };
 
+const sub = (fNum, sNum) => {
+  return fNum - sNum;
+};
+
 module.exports = sum;
--- main.js
+++ main.js
@@ -6,3 +6,5 @@ console.log(sum(234, 432));
 // git add .  - to select all files to be commited
 // git commit -m "message"  - to save the file to git
 // git log
+// git checkout commit id
+// git checkout master
